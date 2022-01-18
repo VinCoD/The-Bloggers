593afe0206d8b4ec8b867021818e85448dc27a64
VinCoD <5895.2020@students.ku.ac.ke>
2022-01-18 09:50:06 +0300

    Made changes to the models and views page

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -9,18 +9,18 @@ def index(request):
     return render(request, 'learning_logs/index.html')
 # @login_required
 def topics(request):
-    topics = Topic.objects.filter().order_by('date_added')
+    topics = Headline.objects.filter().order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
 # @login_required
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
-    topic = Topic.objects.get(id=topic_id)
+    topic = Headline.objects.get(id=topic_id)
     # Make sure the topic belongs to the current user
     # if topic.owner != request.user:
     #     raise Http404
-    entries = topic.entry_set.order_by('-date_added')
+    entries = topic.post_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
@@ -29,10 +29,10 @@ def new_topic(request):
     """Add a new topic"""
     if request.method != 'POST':
         # No data submitted; create a blank form.
-        form = TopicForm()
+        form = HeadlineForm()
     else:
         # POST data submitted; process data.
-        form = TopicForm(data=request.POST)
+        form = HeadlineForm(data=request.POST)
         if form.is_valid():
             new_topic= form.save(commit=False)
             new_topic.owner = request.user
@@ -46,14 +46,14 @@ def new_topic(request):
 @login_required
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic."""
-    topic = Topic.objects.get(id=topic_id)
+    topic = Headline.objects.get(id=topic_id)
 
     if request.method != 'POST':
         # No data submitted; create a blank form.
-        form = EntryForm()
+        form = PostForm()
     else:
         # POST data submitted; process data.
-        form = EntryForm(data=request.POST)
+        form = PostForm(data=request.POST)
         if form.is_valid():
             new_entry = form.save(commit=False)
             new_entry.topic = topic
@@ -67,16 +67,16 @@ def new_entry(request, topic_id):
 @login_required
 def edit_entry(request, entry_id):
     """Edit an existing entry."""
-    entry = Entry.objects.get(id=entry_id)
+    entry = Post.objects.get(id=entry_id)
     topic = entry.topic
 
     if request.method != 'POST':
         # Initial request; pre-fill with current entry.
-        form = EntryForm(instance=entry)
+        form = PostForm(instance=entry)
 
     else:
         # POST data submitted; process data
-        form = EntryForm(instance=entry, data=request.POST)
+        form = PostForm(instance=entry, data=request.POST)
         if form.is_valid():
             form.save()
             return redirect('learning_logs:topic', topic_id=topic.id)
